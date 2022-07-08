d27299fe61f951ede29205e189eb12917b2b756f
Jiajie Chen <jiegec@qq.com>
2022-07-08 21:04:22 +0800

    Add tests for bonding

--- test_convert.py
+++ test_convert.py
@@ -22,3 +22,23 @@ def test_simple():
     assert network['Network']['Gateway'] == '192.168.0.1'
     assert network['Link']['MTUBytes'] == '1024'
     assert network['Link']['MACAddress'] == '00:11:22:33:44:55'
+
+def test_bonding():
+    config = '''
+    auto bond0
+    iface bond0 inet static
+        bond-slaves eth1 eth2
+    '''
+
+    f = io.StringIO(config)
+
+    result = convert.convert_file(f, convert.AutoVivification())
+    netdev = result['bond0.netdev']
+    assert netdev['NetDev']['Name'] == 'bond0'
+    assert netdev['NetDev']['Kind'] == 'bond'
+
+    network = result['eth1.network']
+    assert network['Network']['Bond'] == 'bond0'
+
+    network = result['eth2.network']
+    assert network['Network']['Bond'] == 'bond0'

1b379a72767bdd4dd004f42095aeda9ac9c5e618
Jiajie Chen <c@jia.je>
2023-07-31 09:13:17 +0800

    Support multiple address and gateway

--- migrate_to_systemd_networkd/ifupdown.py
+++ migrate_to_systemd_networkd/ifupdown.py
@@ -63,26 +63,40 @@ class Converter:
         result[network]["Match"]["Name"] = name
 
         # Configs
-        if "address" in config and "netmask" in config:
-            # address and netmask
-            netmask = ipaddress.IPv4Network("0.0.0.0/{}".format(config["netmask"][0]))
-            result[network]["Address"]["Address"] = "{}/{}".format(
-                config["address"][0], netmask.prefixlen
-            )
-        elif "address" in config:
-            # only address
-            result[network]["Address"]["Address"] = config["address"][0]
+        if "address" in config:
+            address_config = AutoVivification()
+            if "netmask" in config:
+                # address and netmask
+                netmask = ipaddress.IPv4Network(
+                    "0.0.0.0/{}".format(config["netmask"][0]))
+                address = "{}/{}".format(
+                    config["address"][0], netmask.prefixlen
+                )
+            else:
+                # only address
+                address = config["address"][0]
+            address_config["Address"] = address
 
-        if method == "static":
-            if "scope" in config:
-                result[network]["Address"]["Scope"] = config["scope"][0]
-            if "pointopoint" in config:
-                result[network]["Address"]["Peer"] = config["pointopoint"][0]
-            if "metric" in config:
-                result[network]["Address"]["RouteMetric"] = config["metric"][0]
+            if method == "static":
+                if "scope" in config:
+                    address_config["Scope"] = config["scope"][0]
+                if "pointopoint" in config:
+                    address_config["Peer"] = config["pointopoint"][0]
+                if "metric" in config:
+                    address_config["RouteMetric"] = config["metric"][0]
+
+            if "Address" in result[network]:
+                result[network]["Address"].append(address_config)
+            else:
+                result[network]["Address"] = [address_config]
 
         if "gateway" in config:
-            result[network]["Network"]["Gateway"] = config["gateway"][0]
+            gateway = config["gateway"][0]
+            if "Gateway" in result[network]["Network"]:
+                result[network]["Network"]["Gateway"].append(gateway)
+            else:
+                result[network]["Network"]["Gateway"] = [gateway]
+
         if "hwaddress" in config:
             value = config["hwaddress"][0]
             parts = value.split(" ")
@@ -96,7 +110,7 @@ class Converter:
         if "." in name:
             index = name.rfind(".")
             device = name[:index]
-            vlan_id = int(name[index + 1 :])
+            vlan_id = int(name[index + 1:])
 
             result[netdev]["NetDev"]["Name"] = name
             result[netdev]["NetDev"]["Kind"] = "vlan"
@@ -139,7 +153,8 @@ class Converter:
                 "balance-tlb",
                 "balance-alb",
             ]
-            result[netdev]["Bond"]["Mode"] = policy[int(config["bond-mode"][0])]
+            result[netdev]["Bond"]["Mode"] = policy[int(
+                config["bond-mode"][0])]
         if "bond-miimon" in config:
             result[netdev]["Bond"]["MIIMonitorSec"] = (
                 float(config["bond-miimon"][0]) / 1000
@@ -401,7 +416,8 @@ def run():
         help="output config for systemd-networkd service config, default to /etc/systemd/networkd.conf.d/tables.conf",
         default="/etc/systemd/networkd.conf.d/tables.conf",
     )
-    parser.add_argument("--systemd-version", required=False, help="systemd version")
+    parser.add_argument("--systemd-version", required=False,
+                        help="systemd version")
     args = parser.parse_args()
 
     converter = Converter(
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -23,8 +23,8 @@ def test_simple():
     result = converter.convert_file(f, convert.AutoVivification())
     network = result["eth0.network"]
     assert network["Match"]["Name"] == "eth0"
-    assert network["Address"]["Address"] == "192.168.0.100/24"
-    assert network["Network"]["Gateway"] == "192.168.0.1"
+    assert network["Address"][0]["Address"] == "192.168.0.100/24"
+    assert network["Network"]["Gateway"][0] == "192.168.0.1"
     assert network["Link"]["MTUBytes"] == "1024"
     assert network["Link"]["MACAddress"] == "00:11:22:33:44:55"
 
@@ -66,7 +66,7 @@ def test_vlan():
     assert network["Network"]["VLAN"] == ["eth0.123"]
 
     network = result["eth0.123.network"]
-    assert network["Address"]["Address"] == "192.168.0.1/24"
+    assert network["Address"][0]["Address"] == "192.168.0.1/24"
 
     netdev = result["eth0.123.netdev"]
     assert netdev["NetDev"]["Name"] == "eth0.123"
@@ -116,3 +116,42 @@ def test_duplicate_vlan():
     network = result["eth0.network"]
     assert network["Match"]["Name"] == "eth0"
     assert network["Network"]["VLAN"] == ["eth0.123"]
+
+
+def test_inet6():
+    config = """
+    auto eth0
+    iface eth0 inet6 static
+        address fec0:0:0:1::2/64
+        gateway fec0:0:0:1::1
+    """
+
+    f = io.StringIO(config)
+    converter = create_converter()
+    result = converter.convert_file(f, convert.AutoVivification())
+    network = result["eth0.network"]
+    assert network["Match"]["Name"] == "eth0"
+    assert network["Address"][0]["Address"] == "fec0:0:0:1::2/64"
+    assert network["Network"]["Gateway"][0] == "fec0:0:0:1::1"
+
+
+def test_inet4_and_6():
+    config = """
+    auto eth0
+    iface eth0 inet static
+        address 192.168.0.100
+        netmask 255.255.255.0
+        gateway 192.168.0.1
+    iface eth0 inet6 static
+        address fec0:0:0:1::2/64
+        gateway fec0:0:0:1::1
+    """
+
+    f = io.StringIO(config)
+    converter = create_converter()
+    result = converter.convert_file(f, convert.AutoVivification())
+    network = result["eth0.network"]
+    assert network["Match"]["Name"] == "eth0"
+    assert network["Address"][0]["Address"] == "192.168.0.100/24"
+    assert network["Address"][1]["Address"] == "fec0:0:0:1::2/64"
+    assert network["Network"]["Gateway"] == ["192.168.0.1", "fec0:0:0:1::1"]

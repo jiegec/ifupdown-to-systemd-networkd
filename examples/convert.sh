599b87c7c57e88fc45dd47368bb246f5ada6f661
Jiajie Chen <c@jia.je>
2023-07-31 09:18:17 +0800

    Add static-v6 example

--- examples/convert.sh
+++ examples/convert.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-for folder in bonding post-up simple static vlan dhcp; do
+set -e
+for folder in bonding post-up simple static static-v6 vlan dhcp; do
     yes | poetry run ifupdown-to-systemd-networkd --interfaces $folder/interfaces --output $folder --tables $folder/rt_tables --config $folder/tables.conf --systemd-version 248
 done
